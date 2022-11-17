35b95a052ca3ee3d6b114f6b2a05d41ae1c6b7c3
Carlos Valenzuela <luzbelshin@gmail.com>
2022-11-17 14:58:37 -0600

    mongodb changes

--- src/app.js
+++ src/app.js
@@ -81,7 +81,7 @@ app.use((req, res, next) => {
 app.use(require('./routes/index'));
 app.use(require('./routes/schedule'));
 app.use(require('./routes/mail'));
-app.use(require('./routes/users'));
+app.use(require('./routes/user'));
 
 // Static Files
 app.use(express.static(path.join(__dirname, 'public')));
--- src/database.js
+++ src/database.js
@@ -13,4 +13,4 @@ client.connect(err => {
 
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/controlapp').then(db => console.log('DB is connected')).catch(err => console.error(err));
+mongoose.connect(process.env.MONGODBURI).then(db => console.log('DB is connected')).catch(err => console.error(err));
